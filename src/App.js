6f00e514888e6e4618c71a432565346624d7928d
kiranFullStack <kiran.fullstack@outlook.com>
2023-03-05 21:43:38 +0530

    added multiple components

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-import logo from './logo.svg'
 import './App.css'
+import Component from './Component'
+import Component1 from './Component1'
+import Footer from './Footer'
+import Header from './Header'
 
 function App() {
   return (
     <div className='App'>
-      <header className='App-header'>
-        <img
-          src={logo}
-          className='App-logo'
-          alt='logo'
-        />
-        <p>Kiran's first app - version 3</p>
-        <a
-          className='App-link'
-          href='https://reactjs.org'
-          target='_blank'
-          rel='noopener noreferrer'>
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <Component1 />
+      <Component />
+      <Footer />
     </div>
   )
 }
---
+++ src/Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Footer from './Footer'
+
+export default function Header() {
+  return (
+    <div>
+      <h1>Header</h1>
+      <Footer />
+    </div>
+  )
+}
